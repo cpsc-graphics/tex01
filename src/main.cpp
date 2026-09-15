19800eec845b69201e695edcf6a0eee4f5fd1af1
ualim <ualim@ucalgary.ca>
2026-09-15 13:18:36 -0600

    Solution

--- src/main.cpp
+++ src/main.cpp
@@ -33,9 +33,8 @@ void renderingThread(sf::RenderWindow& window, std::atomic<bool>& running, std::
 		
         window.clear();
 
-        // TO DO
-        // Draw the ship by calling the appropriate draw method on the window.    
-        // ...
+        // draw the ship    
+        window.draw( *ship );
         
         // end the current frame
         window.display();
--- src/ship.cpp
+++ src/ship.cpp
@@ -1,13 +1,16 @@
 #include "ship.h"
 
 Ship::Ship() : vertices(sf::PrimitiveType::TriangleStrip, 4)  {
-    // TO DO
-    // Add vertices to the vertex array to form a triangle strip that represents the ship.
-    // ...
-    
-    // TO DO
-    // Define the color of the ship. All vertices get the same color. You can make the ship red.
-    // ...
+    // define the positions
+    vertices[0].position = sf::Vector2f(-0.7071f, -0.7071f);
+    vertices[1].position = sf::Vector2f(1.0f, 0.0f);;
+    vertices[2].position = sf::Vector2f(0.0f, 0.0f);
+    vertices[3].position = sf::Vector2f(-0.7071f, 0.7071f);
+
+    // define the color of the ship. All vertices get the same color
+    for (std::size_t i = 0; i < vertices.getVertexCount(); ++i) {
+        vertices[i].color = sf::Color::Red;
+    }
 }
 
 void Ship::draw(sf::RenderTarget& target, sf::RenderStates states) const {   
